1b415b27c24f6c1a9b98609ea6651befc3b36e10
ALI <alirezaei.eng7@gmail.com>
2025-07-03 18:37:27 +0330

    complete services

--- components/shared/ServicesSection.tsx
+++ components/shared/ServicesSection.tsx
@@ -1,6 +1,5 @@
 import { services } from "@/lib/constants";
-import { Button } from "../ui/button";
-import Link from "next/link";
+import ServiceItem from "./ServiceItem";
 
 function ServicesSection() {
   return (
@@ -10,31 +9,14 @@ function ServicesSection() {
           {services.map((service, index) => {
             const Icon = service.icon;
             return (
-              <div
+              <ServiceItem
                 key={index}
-                className="group bg-background border-border hover:border-primary dark:bg-muted rounded-2xl border p-6 shadow transition hover:shadow-lg"
-              >
-                <div className="mb-4 flex justify-center">
-                  <div className="bg-primary/10 text-primary group-hover:bg-primary rounded-full p-3 transition group-hover:text-white">
-                    <Icon className="h-6 w-6 dark:text-neutral-900" />
-                  </div>
-                </div>
-                <h3 className="mt-4 mb-2 text-lg font-bold dark:text-neutral-200">
-                  {service.title}
-                </h3>
-                <p className="text-muted-foreground mt-4 mb-2 text-sm leading-6">
-                  {service.description}
-                </p>
-                <div className="flex items-center justify-center">
-                  <Button
-                    variant="outline"
-                    asChild
-                    className="rounded-full px-6 py-4 text-sm font-semibold text-neutral-900 transition hover:text-neutral-700 md:px-8 md:py-6 dark:text-white dark:hover:text-neutral-200"
-                  >
-                    <Link href={service.href}>{service.btnText}</Link>
-                  </Button>
-                </div>
-              </div>
+                title={service.title}
+                description={service.description}
+                href={service.href}
+                btnText={service.btnText}
+                Icon={Icon}
+              />
             );
           })}
         </div>
