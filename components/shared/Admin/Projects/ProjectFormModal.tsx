2796258f5c99e78d0363cb7ef64f76dd7782b3e0
ALI <alirezaei.eng7@gmail.com>
2025-07-05 19:09:40 +0330

    add projectFormModal

---
+++ components/shared/Admin/Projects/ProjectFormModal.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { InsertProjectValues } from "@/types";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogHeader,
+} from "@/components/ui/dialog";
+import ProjectForm from "./ProjectForm";
+
+type ProjectFormModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  type: "create" | "edit";
+  initialData?: InsertProjectValues;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  categories: any[];
+};
+
+function ProjectFormModal({
+  isOpen,
+  onClose,
+  type,
+  initialData,
+  categories,
+}: ProjectFormModalProps) {
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="max-w-2xl">
+        <DialogHeader>
+          <DialogTitle className="mt-4 mb-2 text-right">
+            {type === "create" ? "ایجاد پروژه جدید" : "ویرایش پروژه"}
+          </DialogTitle>
+        </DialogHeader>
+
+        <ProjectForm
+          onClose={onClose}
+          initialData={initialData}
+          categories={categories}
+          type={type}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ProjectFormModal;
