59bad0a7c8c48824ca637389e9743cbb5c433002
ALI <alirezaei.eng7@gmail.com>
2025-07-05 18:49:25 +0330

    add navbar merged

---
+++ components/shared/Header/NavBar.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import ServicesMenu from "./ServicesMenu";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul className="hidden items-center gap-6 md:flex rtl:space-x-reverse">
+        <li>
+          <ServicesMenu />
+        </li>
+        <li>
+          <Link href="/about" className="hover:text-neutral-200">
+            درباره ما
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className="hover:text-neutral-200">
+            تماس با ما
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
