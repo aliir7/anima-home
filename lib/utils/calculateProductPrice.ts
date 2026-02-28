ff4d43433d7c1102fd0295f3142fac04a1192efc
ALI <alirezaei.eng7@gmail.com>
2026-02-28 17:45:22 +0330

    edit CalculateProductPrice

--- lib/utils/calculateProductPrice.ts
+++ lib/utils/calculateProductPrice.ts
@@ -1,14 +1,14 @@
 export function calculateProductPrice(price: number, discountPercent?: number) {
   const productPrice = price;
-  const taxPrice = 0.1 * productPrice;
-  const benefitsPercent = 0.15 * productPrice;
-  let totalPrice = productPrice + taxPrice + benefitsPercent;
+  const taxPrice = 0.1 * productPrice + 8000;
+
+  let totalPrice = productPrice + taxPrice;
   if (discountPercent !== 0) {
     const discountedPrice = Math.round(
       productPrice * (1 - discountPercent! / 100),
     );
 
-    totalPrice = discountedPrice + taxPrice + benefitsPercent;
+    totalPrice = discountedPrice + taxPrice;
 
     return totalPrice;
   }
