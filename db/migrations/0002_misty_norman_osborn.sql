e67d6432bef97dbd5b2d71732fbffe175c22745f
ALI <alirezaei.eng7@gmail.com>
2026-09-11 13:14:49 +0330

    Remove obsolete migration files for coupons, reviews, and site settings; delete journal.json.

--- db/migrations/0002_misty_norman_osborn.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "order_items" DROP CONSTRAINT "order_items_order_id_product_id_pk";--> statement-breakpoint
-ALTER TABLE "order_items" ADD COLUMN "id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL;--> statement-breakpoint
-ALTER TABLE "categories" ADD CONSTRAINT "categories_parent_id_categories_id_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."categories"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "product_categories" ADD CONSTRAINT "product_categories_parent_id_product_categories_id_fk" FOREIGN KEY ("parent_id") REFERENCES "public"."product_categories"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "product_categories" ADD CONSTRAINT "product_categories_name_unique" UNIQUE("name");
